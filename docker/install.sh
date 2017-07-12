f3d355d00ef05934601d981b8ea4ae9b65efaf20
Jon Jagger <jon@jaggersoft.com>
2017-07-12 11:32:00 +0100

    added [set -e] to install.sh to ensure if it fails [docker build] fails

--- docker/install.sh
+++ docker/install.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+set -e
 
 # Install gcc 7.1
 apt-get update
-apt-get install build-essential software-properties-common -y
-add-apt-repository ppa:ubuntu-toolchain-r/test -y
+apt-get install --yes build-essential software-properties-common
+add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 apt-get update
-apt-get install gcc-7 g++-7 -y
+apt-get install --yes gcc-7 g++-7
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 60 --slave /usr/bin/g++ g++ /usr/bin/g++-7
 update-alternatives --config gcc
 
 # Install boost
-apt-get install -y wget unzip
+apt-get install --yes wget unzip
 wget https://dl.bintray.com/boostorg/release/1.64.0/source/boost_1_64_0.tar.bz2
 tar --bzip2 -xf boost_1_64_0.tar.bz2
 cd boost_1_64_0
@@ -36,3 +37,5 @@ wget https://raw.githubusercontent.com/meekrosoft/fff/master/fff.h
 
 # Update the Dynamic Linker Run Time Bindings
 ldconfig
+
+apt-get remove --yes wget unzip
