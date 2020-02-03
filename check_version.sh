2b89221ad37e3dc90ced7aaefb89f54a429e71d6
Jon Jagger <jon@jaggersoft.com>
2020-02-03 10:59:02 +0000

    Refactoring; in check-version.sh get image-name from json file

--- check_version.sh
+++ check_version.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -Eeu
 
+readonly REGEX="image_name\": \"(.*)\""
+readonly JSON=`cat docker/image_name.json`
+[[ ${JSON} =~ ${REGEX} ]]
+readonly IMAGE_NAME="${BASH_REMATCH[1]}"
+
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 readonly EXPECTED=9.2.0
-readonly ACTUAL=$(docker run --rm -it cyberdojofoundation/gplusplus sh -c 'c++ --version')
+readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c 'c++ --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
   echo "VERSION CONFIRMED as ${EXPECTED}"
